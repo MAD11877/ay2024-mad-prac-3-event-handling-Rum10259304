5899e596a1280d4a062b63e46d2b864e26178076
Rafiudeen Rumaisa Asmi <s10259304@connect.np.edu.sg>
2024-06-19 18:27:44 +0800

    weeelllllo

---
+++ app/src/main/java/sg/edu/np/mad/madpractical3/ListActivity.java
@@ -0,0 +1,63 @@
+package sg.edu.np.mad.madpractical3;
+
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Random;
+
+public class ListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_list);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setTitle("Profile");
+        builder.setMessage("MADness");
+        builder.setCancelable(true);
+        builder.setPositiveButton("VIEW", new DialogInterface.OnClickListener(){
+            public void onClick(DialogInterface dialog, int id){
+                Random rand = new Random();
+                int randomNumber = rand.nextInt();
+                Intent activityMain = new Intent(ListActivity.this, MainActivity.class);
+                activityMain.putExtra("Number",""+randomNumber);
+                startActivity(activityMain);
+
+            }
+        });
+        builder.setNegativeButton("CLOSE", new DialogInterface.OnClickListener(){
+            public void onClick(DialogInterface dialog, int id){
+            }
+        });
+
+        ImageView ivprofile = findViewById(R.id.IVprofile);
+
+        ivprofile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog alert = builder.create();
+                alert.show();
+            }
+        });
+    }
+}
